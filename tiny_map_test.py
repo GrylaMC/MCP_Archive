0189e31abd508234d0d717b7a1cd71d58f8425bd
HeronErin <HeronErin@protonmail.com>
2025-12-30 07:23:18 -0500

    All Alpha formats have been added

--- tiny_map_test.py
+++ tiny_map_test.py
@@ -35,6 +35,7 @@ def get_namespaces(mapping_file):
 if __name__ == "__main__":
     with tempfile.TemporaryDirectory(delete=True) as tempdir:
         for diR in sorted(os.listdir("tiny_v1s")):
+            #if not diR.startswith("b"): continue
             full_dir = os.path.join("tiny_v1s", diR)
             # for file in sorted(os.listdir(full_dir)):
             for file in sorted(os.listdir(full_dir)):
